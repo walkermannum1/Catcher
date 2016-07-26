c4fae8ae09aa6d51f0961ef8accfe430f964da19
walkermannum1 <2469377884@qq.com>
2016-07-26 10:26:42 +0800

    Change layout.

--- app/src/main/java/com/example/user/catcher/login/Login.java
+++ app/src/main/java/com/example/user/catcher/login/Login.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -12,13 +15,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.user.catcher.MainActivity.MainActivity;
 import com.example.user.catcher.R;
+import com.example.user.catcher.Register.Register;
 
 
 /**
  * Created by user on 2016/7/14.
  */
-public class Login extends Activity implements View.OnClickListener {
+public class Login extends Activity {
 
     private SharedPreferences pref;
     private SharedPreferences.Editor editor;
@@ -37,6 +42,28 @@ public class Login extends Activity implements View.OnClickListener {
         register = (TextView) findViewById(R.id.register);
         retrpass = (TextView) findViewById(R.id.retrpass);
         rememberPass = (CheckBox) findViewById(R.id.remember_pass);
+        String text1 = "Register";
+        String text2 = "Forgot password";
+        SpannableString spannableString1 = new SpannableString(text1);
+
+        spannableString1.setSpan(new ClickableSpan() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+            }
+        }, 0, text1.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        SpannableString spannableString2 = new SpannableString(text2);
+
+        spannableString2.setSpan(new ClickableSpan() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+            }
+        }, 0, text2.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
         boolean isRemember =pref.getBoolean("remember_password", false);
         if (isRemember) {
             String account = pref.getString("account", "");
