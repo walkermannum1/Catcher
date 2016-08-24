9d7c1d6f76369763ef07646eb2acb46a7ed40ddb
walkermannu1 <24>
2016-08-24 22:53:18 +0800

    Add some code.

---
+++ app/src/main/java/com/example/user/catcher/MainActivity/Welcome.java
@@ -0,0 +1,17 @@
+package com.example.user.catcher.MainActivity;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 24693 on 2016/8/24.
+ */
+public class Welcome extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
